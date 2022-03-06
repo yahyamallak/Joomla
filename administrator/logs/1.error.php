7fe437459f215fca8268d301d103137daa21ddc5
Yahya mallak <Yahya10mallak@gmail.com>
2022-03-06 07:32:25 +0100

    set up joomla

---
+++ administrator/logs/1.error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-02-09 11:04:54 UTC
+#Software: Joomla! 4.0.6 Stable [ Furaha ] 15-January-2022 17:06 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-02-09T11:04:54+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
